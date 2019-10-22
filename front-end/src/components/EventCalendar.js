f1950339fb39207458a6c8297e7226d1ea1d81db
Carl-Stanley <cds.beml@gmail.com>
2019-10-21 20:01:06 -0700

    Commit

---
+++ front-end/src/components/EventCalendar.js
@@ -0,0 +1,31 @@
+import { ScheduleComponent } from '@syncfusion/ej2-react-schedule';
+import { Inject, Month,ViewDirective, ViewsDirective} from '@syncfusion/ej2-react-schedule';
+import * as React from 'react';
+import * as ReactDOM from "react-dom";
+import { Ajax } from '@syncfusion/ej2-base';
+import './App.css';
+import { DataManager, UrlAdaptor } from '@syncfusion/ej2-data';
+
+class EventCalendar extends React.Component {
+  
+  render() {
+    
+    return <ScheduleComponent currentView='Month'>
+       
+    
+     <ViewsDirective>
+
+        <ViewDirective option='Month'/>
+        
+    
+    </ViewsDirective>
+
+   <Inject services={[Month]}/>
+
+</ScheduleComponent>
+
+  }
+
+}
+
+export default EventCalendar;
