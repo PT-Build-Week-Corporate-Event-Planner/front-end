1226c7810d5bc303659c9c5fd8278b8749ecbbef
Jabril Breckenridge <jabriltb2@gmail.com>
2019-10-21 22:11:29 -0500

    Minor change

--- front-end/src/components/Onboard.js
+++ front-end/src/components/Onboard.js
@@ -86,34 +86,3 @@ const SignupForm = props => {
 };
 
 export default SignupForm;
-
-// const Onboarding = ()=>{
-
-// useEffect( () =>{
-//     const registerUser = user => {
-//         axios
-//             .post('/user', {
-//                 FName: user.fName,
-//                 LName: user.lName,
-//                 Email: user.email,
-//                 CompanyName: user.companyName,
-//                 role: user.role
-//             })
-//             .then(response => {
-//                 console.log(response)
-//             })
-//             .catch(error => {
-//                 console.log(error)
-//             })
-//     }
-//     registerUser()
-// }, [])
-
-// return(
-//     <>
-//         <Form>
-//             <h2></h2>
-//         </Form>
-//     </>
-// )
-// }
