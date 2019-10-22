056538b3c022e98bf963d79208c6cad4a99726e1
Jabril Breckenridge <jabriltb2@gmail.com>
2019-10-21 22:06:40 -0500

    Form complete.

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
-// import { Route } from "react-router-dom";
+import { Route } from "react-router-dom";
 import SignupForm from "./components/Onboard";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<SignupForm />
-			{/* <Route path="/signUp" component={Onboard} /> */}
+			<Route path="/signUp" component={SignupForm} />
+			{/* <Route path="/login"  /> */}
 		</div>
 	);
 }
--- front-end/src/components/Onboard.js
+++ front-end/src/components/Onboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import axios from "axios";
 import * as Yup from "yup";
@@ -13,53 +13,77 @@ const SignupSchema = Yup.object().shape({
 	role: Yup.string()
 });
 
-const SignupForm = () => (
+const SignupForm = props => {
+	console.log(props);
+	const registerUser = user => {
+		console.log(user);
+		axios
+			.post("https://corporate-event-planner-webeu.herokuapp.com/api/auth/register", {
+				first_name: user.firstName,
+				last_name: user.lastName,
+				password: user.password,
+				email: user.email,
+				company: user.companyName,
+				role: user.role
+			})
+			.then(response => {
+				// console.log(response);
+				props.history.push("/login");
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	};
 
-    
-	<div>
-		<h2>Become an Event Coordinator today!</h2>
-		<Formik
-			initialValues={{
-				firstName: "",
-				lastName: "",
-				email: "",
-				companyName: "",
-				role: ""
-			}}
-			validationSchema={SignupSchema}
-			onSubmit={values => {
-				console.log(values);
-			}}>
-			{({ isSubmitting }) => (
-				<Form>
-					<div>
-						<Field name="firstName" placeholder="First Name" />
-						<ErrorMessage name="firstName" component="span" />
-					</div>
-					<div>
-						<Field name="lastName" placeholder="Last Name" />
-						<ErrorMessage name="lastName" component="span" />
-					</div>
-					<div>
-						<Field name="email" placeholder="Email" />
-						<ErrorMessage name="email" component="span" />
-					</div>
-					<div>
-						<Field name="companyName" placeholder="Company Name" />
-						<ErrorMessage name="companyName" component="span" />
-					</div>
-					<div>
-						<Field name="role" placeholder="Role" />
-						<ErrorMessage name="role" component="span" />
-					</div>
-					<button type="submit" disabled={isSubmitting}>
-						Submit
-					</button>
-				</Form>
-			)}
-		</Formik>
-	</div>
-);
+	return (
+		<div>
+			<h2>Become an Event Coordinator today!</h2>
+			<Formik
+				initialValues={{
+					firstName: "",
+					lastName: "",
+					password: "",
+					email: "",
+					companyName: "",
+					role: ""
+				}}
+				validationSchema={SignupSchema}
+				onSubmit={registerUser}>
+				{({ isSubmitting }) => (
+					<Form>
+						<div>
+							<Field name="firstName" placeholder="First Name" />
+							<ErrorMessage name="firstName" component="span" />
+						</div>
+						<div>
+							<Field name="lastName" placeholder="Last Name" />
+							<ErrorMessage name="lastName" component="span" />
+						</div>
+						<div>
+							<Field name="email" placeholder="Email" />
+							<ErrorMessage name="email" component="span" />
+						</div>
+						<div>
+							<Field name="password" placeholder="Password" type="password" />
+							<ErrorMessage name="password" component="span" />
+						</div>
+						<div>
+							<Field name="companyName" placeholder="Company Name" />
+							<ErrorMessage name="companyName" component="span" />
+						</div>
+						<div>
+							<Field name="role" placeholder="Role" />
+							<ErrorMessage name="role" component="span" />
+						</div>
+						<button type="submit" disabled={isSubmitting}>
+							Submit
+						</button>
+					</Form>
+				)}
+			</Formik>
+		</div>
+	);
+};
 
 export default SignupForm;
 
