a0dc10853cd011e3de7c3b7f90415a3271ec05d3
Carl-Stanley <cds.beml@gmail.com>
2019-10-21 21:18:28 -0700

    Commit

--- front-end/src/components/eventcalendar/EventCalendar.js
+++ front-end/src/components/eventcalendar/EventCalendar.js
@@ -11,11 +11,31 @@ import axios from 'axios';
 class EventCalendar extends React.Component {
   constructor () {
     super()
-
+    this.data = [{
+      Id: 1,
+      Subject: 'Explosion of Betelgeuse Star',
+      StartTime: new Date(2019, 9, 15, 9, 30),
+      EndTime: new Date(2019, 9, 15, 11, 0)
+  }, {
+      Id: 2,
+      Subject: 'Thule Air Crash Report',
+      StartTime: new Date(2019, 9, 12, 12, 0),
+      EndTime: new Date(2019, 9, 12, 14, 0)
+  }, {
+      Id: 3,
+      Subject: 'Blue Moon Eclipse',
+      StartTime: new Date(2019, 9, 13, 9, 30),
+      EndTime: new Date(2019, 9, 13, 11, 0)
+  }, {
+      Id: 4,
+      Subject: 'Meteor Showers in 2018',
+      StartTime: new Date(2019, 9, 14, 13, 0),
+      EndTime: new Date(2019, 9, 14, 14, 30)
+  }];
   } 
   render() {
     
-    return ( <div className='mainCalendar'><ScheduleComponent currentView='Month'>       
+    return ( <div className='mainCalendar'><ScheduleComponent currentView='Month' height='600px' eventSettings={{ dataSource: this.data }}>       
     
   <ViewsDirective>
 
