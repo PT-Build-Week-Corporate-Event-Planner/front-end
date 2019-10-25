1f1c8d8a12efce33bb466a9ee4c1019b645d095c
Carl-Stanley <cds.beml@gmail.com>
2019-10-24 17:12:48 -0700

    Commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import SignupForm from "./components/Onboard";
-import TasksList from "./components/TasksList";
-
+import EventsList from './components/EventsList';
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<TasksList />
-			<Route path="/register" component={SignupForm} />
-			{/* <Route path="/login"  /> */}
+			<EventsList />
 		</div>
 	);
 }
