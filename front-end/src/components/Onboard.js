e7c1427e8b548835d578f483c117b70179d10314
Eric <eric@Erics-MacBook-Pro.local>
2019-10-27 21:39:53 -0500

    Styled Login and Signup components

--- front-end/src/components/Onboard.js
+++ front-end/src/components/Onboard.js
@@ -2,6 +2,62 @@ import React from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import axios from "axios";
 import * as Yup from "yup";
+import styled from 'styled-components';
+
+const SignUpHeader = styled.h1`
+color: black;
+text-align: center;
+font-size: 2.5rem;
+padding: 2rem;
+`;
+
+const StylForm = styled.form`
+    display: flex;
+    flex-direction: column;
+    width: 25%;
+    margin: 0 auto;
+    background: #efeff3;
+    padding: 2rem 2rem;
+    border-radius: 5px;
+    box-shadow: #999 1px 2px 5px;
+	
+	label {
+		font-size 1.5rem;
+		margin-right: 10px;
+	}
+
+    input,textarea,select {		
+        font-size: 1rem;
+        margin-bottom: 1rem;
+        border: 1px solid gray;
+        border-radius: 3px;
+        height: 30px;
+        padding-left: 12px;
+        outline: none;
+        &::placeholder {
+            color: gray;
+	}	
+	
+  }
+  textarea {
+    width: 500px;
+    height: 150px;
+  }
+  button {
+    padding: 0.5rem 0.5rem;
+    background: #323232;
+    color: #fff;
+    font-size: 1rem;
+    cursor: pointer;
+    outline: none;
+    border: none;
+    border-radius: 3px;
+    &:hover {
+      color: #323232;
+      background: #fff;
+    }
+  } 
+`;
 
 const SignupSchema = Yup.object().shape({
 	firstName: Yup.string().required("A first name is required"),
@@ -37,7 +93,7 @@ const SignupForm = props => {
 
 	return (
 		<div>
-			<h2>Become an Event Coordinator today!</h2>
+			<SignUpHeader>Become an Event Coordinator today!</SignUpHeader>
 			<Formik
 				initialValues={{
 					firstName: "",
@@ -50,35 +106,41 @@ const SignupForm = props => {
 				validationSchema={SignupSchema}
 				onSubmit={registerUser}>
 				{({ isSubmitting }) => (
-					<Form>
+					<StylForm>
 						<div>
+							<label>First Name</label>
 							<Field name="firstName" placeholder="First Name" />
 							<ErrorMessage name="firstName" component="span" />
 						</div>
 						<div>
+							<label>Last Name</label>
 							<Field name="lastName" placeholder="Last Name" />
 							<ErrorMessage name="lastName" component="span" />
 						</div>
 						<div>
+							<label>Email</label>
 							<Field name="email" placeholder="Email" />
 							<ErrorMessage name="email" component="span" />
 						</div>
 						<div>
+							<label>Password</label>
 							<Field name="password" placeholder="Password" type="password" />
 							<ErrorMessage name="password" component="span" />
 						</div>
 						<div>
+							<label>Company Name</label>	
 							<Field name="companyName" placeholder="Company Name" />
 							<ErrorMessage name="companyName" component="span" />
 						</div>
 						<div>
+							<label>Role</label>
 							<Field name="role" placeholder="Role" />
 							<ErrorMessage name="role" component="span" />
 						</div>
 						<button type="submit" disabled={isSubmitting}>
 							Submit
 						</button>
-					</Form>
+					</StylForm>
 				)}
 			</Formik>
 		</div>
